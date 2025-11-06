1ee230bc2aa210231affb7449515e68c60c3fcaf
Umar Codabaccus <Umar.Codabaccus@dayforce.com>
2025-11-06 11:01:20 +0400

    Add project files.

---
+++ CodeNest.Api/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
